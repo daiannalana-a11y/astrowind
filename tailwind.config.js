b0a79935b07300d2d9c09066a7ecd63a477cf504
daiannalana-a11y <daiannalana@gmail.com>
2026-04-21 22:22:32 -0300

    Define fade animation and keyframes in Tailwind config
    Added custom animation and keyframes for fade-in effect.

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,17 @@ export default {
         serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
         heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
       },
+
+      animation: {
+        fade: 'fadeInUp 1s both',
+      },
+
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(2rem)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
     },
   },
   plugins: [
@@ -26,4 +37,5 @@ export default {
       addVariant('intersect', '&:not([no-intersect])');
     }),
   ],
+  darkMode: 'class',
 };
